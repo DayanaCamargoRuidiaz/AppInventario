9ddbcdfd6dd40d5756ded96b0db73c683b83a574
Usuario <Usuario@10.100.100.1>
2018-07-21 15:05:03 -0500

    Se agrega la capa del modelo y dao

---
+++ src/main/java/co/com/dao/imp/ProductoDaoImpl.java
@@ -0,0 +1,42 @@
+package co.com.dao.imp;
+
+import co.com.dao.IProductoDao;
+import co.com.data.ProductoData;
+import co.com.model.Producto;
+import java.util.List;
+
+public class ProductoDaoImpl implements IProductoDao {
+    
+    @Override
+    public List<Producto> obtenerProductos() {
+        return ProductoData.getListado();
+    }
+    
+    @Override
+    public Producto obtenerProducto(String codigo) {
+        List<Producto> listado = ProductoData.getListado();
+        return listado.get(listado.indexOf(new Producto(codigo)));
+    }
+    
+    @Override
+    public void crearProducto(Producto producto) {
+        List<Producto> listado = ProductoData.getListado();
+        listado.add(producto);
+        ProductoData.setListado(listado);
+        
+    }
+    
+    @Override
+    public void eliminarProducto(String codigo) {
+        List<Producto> listado = ProductoData.getListado();
+        listado.remove(new Producto(codigo));
+        ProductoData.setListado(listado);
+    }
+    
+    
+    @Override
+    public void actualizarProducto(Producto producto) {
+        
+    }
+    
+}
